e3558f49582c37281ac35563252c0e7704cbc197
Segoga <129649003+seg0ga@users.noreply.github.com>
2024-10-28 11:04:02 +0700

    Create 1.py

---
+++ lab4/1.py
@@ -0,0 +1,17 @@
+s = []
+digit = []
+alpha = []
+a = "1"
+while a != "":
+    a = input()
+    if a != "" :s.append(a)
+
+while len(s) != 0:
+    if str(s[0]).isdigit():
+        digit.append(s[0])
+    else:
+        alpha.append(s[0])
+    s.pop(0)
+print(digit)
+print(alpha)
+print(s)
