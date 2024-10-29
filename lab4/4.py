fb61dfedab33bb4b6e1044a0494e3cbfd0e44679
Segoga <129649003+seg0ga@users.noreply.github.com>
2024-10-29 12:00:14 +0700

    Update 4.py

--- lab4/4.py
+++ lab4/4.py
@@ -1,19 +1,15 @@
-s = []
+s = input().split()
 bol = []
 men = []
 rav = []
-a = 1
 sum_all = 0
-while a != "":
-    a = input()
-    if a != "":
-        s.append(int(a))
-        sum_all+=int(a)
+for  i in s:
+    sum_all+=int(i)
 sr_znach = sum_all/len(s)
 for i in s:
-    if i < sr_znach:men.append(str(i))
-    elif i > sr_znach:bol.append(str(i))
-    else:rav.append(str(i))
+    if int(i) < sr_znach:men.append(str(i))
+    elif int(i) > sr_znach:bol.append(str(i))
+    else: rav.append(str(i))
 print("Меньше: "," ".join(men))
 print("Больше: "," ".join(bol))
 if len(rav) > 0: print("Равно: "," ".join(rav))
