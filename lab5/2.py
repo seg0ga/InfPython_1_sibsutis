20ce333b057b0f4de0c04e1de5d421d09f8d7730
Segoga <129649003+seg0ga@users.noreply.github.com>
2024-11-11 14:28:41 +0700

    Update 2.py

--- lab5/2.py
+++ lab5/2.py
@@ -1,17 +1,25 @@
 import random as r
-magic_kv = [["2","7","6"],
-            ["9","5","1"],
-            ["4","3","8"]]
 
-if r.randint(0,1):
-    s = magic_kv[0]
-    magic_kv[0]=magic_kv[2]
-    magic_kv[2]=s
-if r.randint(0,1):
-    s = [magic_kv[0][2],magic_kv[1][2],magic_kv[2][2]]
+while True:
+    m = []
     for i in range(3):
-        magic_kv[i][2]=magic_kv[i][0]
-        magic_kv[i][0] = s[i]
+        m.append([0] * 3)
 
-for i in magic_kv:
-    print(" ".join(i))
+    number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for i in range(3):
+        for j in range(3):
+            m[i][j] = r.choice(number)
+            number.remove(m[i][j])
+    if m[2][0] + m[1][1] + m[0][2] == 15 and \
+            m[0][0] + m[1][1] + m[2][2] == 15 and \
+            m[0][0] + m[0][1] + m[0][2] == 15 and \
+            m[1][0] + m[1][1] + m[1][2] == 15 and \
+            m[2][0] + m[2][1] + m[2][2] == 15 and \
+            m[0][0] + m[1][0] + m[2][0] == 15 and \
+            m[0][1] + m[1][1] + m[2][1] == 15 and \
+            m[0][2] + m[1][2] + m[2][2] == 15:
+        for i in range(3):
+            for j in range(3):
+                print(m[i][j], end=" ")
+            print()
+        break
