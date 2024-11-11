2f6d2709cd42e917f0207942749298809be023ea
Segoga <129649003+seg0ga@users.noreply.github.com>
2024-11-11 14:25:37 +0700

    Create 2.py

---
+++ lab6/2.py
@@ -0,0 +1,21 @@
+e1 = open("file5.txt","r",encoding="utf8")
+k1 = e1.readlines()
+e2 = open("file6.txt","r",encoding="utf8")
+k2 = e2.readlines()
+flag5 = 0
+flag6 = 0
+for i in k1:
+    if "Academy" in i:
+        flag5 = 1
+for i in k2:
+    if "Academy" in i:
+        flag6 = 1
+
+if flag5 == 1:
+    print("В пятом")
+elif flag6 == 1:
+    print("В шестом")
+else:
+    print("В файлах нет слова")
+e1.close()
+e2.close()
